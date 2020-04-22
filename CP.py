4dbd9743c66bf6930786d4ababcf853f93b48e58
Chanchal Kumar Maji <31502077+ChanchalKumarMaji@users.noreply.github.com>
2020-04-22 21:41:27 +0530

    Add CP.py

---
+++ CP.py
@@ -0,0 +1,23 @@
+# python3
+
+##=====================================================
+from sys import stdin, stdout
+from collections import defaultdict, Counter
+from functools import lru_cache
+from math import gcd, floor, ceil
+ 
+def ilist():
+    return [int(x) for x in stdin.readline().strip().split(" ")]
+def iint():
+    return int(stdin.readline().strip())
+def istr():
+    return stdin.readline().strip()
+##=====================================================
+
+
+def solve():
+    pass
+
+
+if __name__ == '__main__':
+    
